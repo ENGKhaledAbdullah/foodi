1a054ed96b6ce9f10405fac7ded8e7894e1bb206
ENGKhaledAbdullah <eng.khaled.alhamedi@gmail.com>
2023-10-17 22:22:10 +0300

    added jquery file and fix some layout errors , apply few media queries for laptops and normal screens.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
     
         let dishCards = document.getElementsByClassName('dishes-cards')[0];
         let cardWidth = document.querySelector('.card').offsetWidth;
-        let scrollDistance = dishCards.clientWidth + 90;
+        // let scrollDistance = dishCards.clientWidth + 90;
+        let scrollDistance=cardWidth+50;
 
         function next() {
         dishCards.scrollBy({ 
@@ -16,3 +17,30 @@
             behavior: 'smooth'
         });
         }
+
+
+        let lastScrollTop = 0;
+        let navbar=document.getElementsByClassName("navbar")[0];
+        window.addEventListener("scroll",function(){
+            let scrollTop= window.scrollY || this.document.documentElement.scrollTop;
+            if (scrollTop>lastScrollTop){
+                navHide();
+            }
+            else{   
+                navbar.style.top="0";
+                setTimeout(navHide,8000);
+            }
+            lastScrollTop=scrollTop;
+
+            function navHide(){
+                navbar.style.top="-80px";
+            }
+        })
+        
+        // $(window).scrollBy(function() {
+        //     if ($(window).scrollTop() >= 50) {
+        //         $('.navbar').css('background', 'red');
+        //     } else {
+        //         $('.navbar').css('background', 'transparent');
+        //     }
+        //     });
